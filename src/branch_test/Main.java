a2d28d0c7778e07078ca93ef300ce43103ad799f
71summernigth <71summernight@naver.com>
2021-03-04 19:14:26 +0900

    hello

--- src/branch_test/Main.java
+++ src/branch_test/Main.java
@@ -6,6 +6,7 @@ public class Main {
         // TODO Auto-generated method stub
            System.out.println("HELLO");
            System.out.println("71summernight");
+           System.out.println("hyunjin");
     }
 
 }
